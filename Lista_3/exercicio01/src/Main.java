9e986734946da2905eb7155f3100a0e4eb5b64b7
Erica Luz <erica.nicollas_hbn@yahoo.com>
2023-04-11 18:28:15 -0300

    Conclusão exercicio1 lista3

--- Lista_3/exercicio01/src/Main.java
+++ Lista_3/exercicio01/src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Heroi heroi1 = new Heroi();
+        //heroi1.nome = "Chapolyn Colorado";
+
+        //Heroi heroi2 = new Heroi();
+        //heroi2.nome = "Gato Félix";
+
+        Inimigo inimigo1 = new Inimigo();
+        inimigo1.nome = "Tripa Seca";
+        inimigo1.ataque = 9;
+
+        Inimigo inimigo2 = new Inimigo();
+        inimigo2.nome = "Poucas trancas";
+        inimigo2.ataque = 8;
+
+        Inimigo inimigo3 = new Inimigo();
+        inimigo3.nome = "Quase Nada";
+        inimigo3.ataque = 10;
+
+        Mapa mapa = new Mapa();
+        mapa.nome = "Mundo 1";
+        //mapa.herois.add(heroi1);
+        //mapa.herois.add(heroi2);
+        mapa.inimigos.add(inimigo1);
+        mapa.inimigos.add(inimigo2);
+        mapa.inimigos.add(inimigo3);
+
+
+        Inimigo inimigoDeMaiorAtaque = mapa.getInimigoMaiorAtaque();
+
+        System.out.println(inimigoDeMaiorAtaque.nome);
+
+
     }
 }
--- Lista_3/exercicio01/src/Mapa.java
+++ Lista_3/exercicio01/src/Mapa.java
@@ -5,6 +5,22 @@ public class Mapa {
     String nome;
     int altura;
     int largura;
-    ArrayList<Inimigo>inimigos;
-    ArrayList<Heroi>herois;
+    ArrayList<Inimigo>inimigos = new ArrayList<>();
+    ArrayList<Heroi>herois = new ArrayList<>();
+
+    public Inimigo getInimigoMaiorAtaque(){
+        int maiorAtaque = 0;
+        int index = 0;
+        for (int i = 0; i < inimigos.size(); i++) {
+            if (inimigos.get(i).ataque > maiorAtaque){
+                maiorAtaque = inimigos.get(i).ataque;
+                index = i;
+            }
+
+        }
+        return inimigos.get(index);
+    }
 }
+//2.32 min array list
+// 2.40 interessante tambem
+//3,25
