6f75a5eb472984215bcf1b4cd5d7647e2a75871e
Erica Luz <erica.nicollas_hbn@yahoo.com>
2023-05-03 23:09:18 -0300

    Algumas implementações no exercicio 01 Herança

--- Lista_6/exercicio01/src/Main.java
+++ Lista_6/exercicio01/src/Main.java
@@ -3,6 +3,11 @@ public class Main {
         Carro gol = new Carro();
         Moto bis = new Moto();
         gol.utilizarHabilidadeEspecial();
+        gol.acelerar();
+        gol.acelerar();
+        gol.ligarFarol(false);
         bis.utilizarHabilidadeEspecial();
+
+
     }
 }
--- Lista_6/exercicio01/src/Veiculo.java
+++ Lista_6/exercicio01/src/Veiculo.java
@@ -16,15 +16,65 @@ public abstract class Veiculo {
     }
     //Metodos Comuns
     public void acelerar(){
-
+        this.setVelocidadeAtual(getVelocidadeAtual() + 5);
+        System.out.println("Você esta acelerando! " + this.getVelocidadeAtual() + " KM");
     }
     public void desacelerar(){
-
+        this.setVelocidadeAtual(getVelocidadeAtual() - 5);
+        System.out.println("Você esta desacelerando!! " + this.getVelocidadeAtual() + " Km");
     }
-    public void ligarFarol(){
+    public void ligarFarol(Boolean estaComfarolLigado){
+        if (estaComfarolLigado){
+            this.setEstaComfarolLigado(estaComfarolLigado);
+            System.out.println("Farol ligado!!!");
+        }else {
+            System.out.println("O farol esta desligado!!!");
+        }
 
     }
     //Metodos abstratos
     //abstract void utilizarHabilidadeEspecial();
     public abstract void utilizarHabilidadeEspecial();
+
+    //Metodos Especiais
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVelocidadeAtual() {
+        return velocidadeAtual;
+    }
+
+    public void setVelocidadeAtual(int velocidadeAtual) {
+        this.velocidadeAtual = velocidadeAtual;
+    }
+
+    public int getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(int velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public int getPrecisaoEmCurvas() {
+        return precisaoEmCurvas;
+    }
+
+    public void setPrecisaoEmCurvas(int precisaoEmCurvas) {
+        this.precisaoEmCurvas = precisaoEmCurvas;
+    }
+
+    public Boolean getEstaComfarolLigado() {
+        return estaComfarolLigado;
+    }
+
+    public void setEstaComfarolLigado(Boolean estaComfarolLigado) {
+        this.estaComfarolLigado = estaComfarolLigado;
+    }
 }
